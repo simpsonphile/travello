afbdf777fbe9151916a382e5f740663b92c05dab
Dawid Mazewski <simpsonphile@gmail.com>
2018-03-08 00:11:39 +0100

    fix

--- js/datapicker.js
+++ js/datapicker.js
@@ -4,6 +4,7 @@ $( function() {
         nextText: ">",
         showAnim: "slideDown",
         beforeShow: function() {
+          $('.ui-datepicker').blur();
         setTimeout(function(){
             $('.ui-datepicker').css('z-index', 99999999999999);
         }, 0);
